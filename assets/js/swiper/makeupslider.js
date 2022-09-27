afec22dfc0d3475e03fb6e7cb3e8b56b0b82d88e
it27427 <istiak.tushar.qtec@gmail.com>
2022-09-27 17:54:50 +0600

    working on services

---
+++ assets/js/swiper/makeupslider.js
@@ -0,0 +1,12 @@
+const makeupSlider = new Swiper(".makeupSlider", {
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  loop: true,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
