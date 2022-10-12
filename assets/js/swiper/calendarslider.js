576af00b67bdd66fd5856c260e12295acddda8b9
it27427 <istiak.tushar.qtec@gmail.com>
2022-10-12 16:30:53 +0600

    booking calendar slot added and completed

--- assets/js/swiper/calendarslider.js
+++ assets/js/swiper/calendarslider.js
@@ -2,10 +2,10 @@ const calendarslide = new Swiper(".calendarslider", {
   grabCursor: true,
   loop: true,
   loopFillGroupWithBlank: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false
-  },
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false
+  // },
   spaceBetween: 16,
   slidesPerGroup: 1,
   navigation: {
