25e2f0e88d100f4ee0b474d24c34a45592283094
it27427 <istiak.tushar.qtec@gmail.com>
2022-10-20 17:00:49 +0600

    appointment completed

--- assets/js/modal/modal.js
+++ assets/js/modal/modal.js
@@ -1,71 +1,16 @@
-const loginModal = document.querySelector('#loginModal');
-const registerModal = document.querySelector('#registerModal');
-const forgotModal = document.querySelector('#forgotModal');
-const changeModal = document.querySelector('#changeModal');
+const manageModal = document.querySelector('#manageModal');
+const cancelModal = document.querySelector('#cancelManageModal');
 
 // INITIALIZING MODAL TO BOOTSTARP
-const login = new bootstrap.Modal(loginModal);
-const register = new bootstrap.Modal(registerModal);
-const forgot = new bootstrap.Modal(forgotModal);
-const change = new bootstrap.Modal(changeModal);
+const manage = new bootstrap.Modal(manageModal);
+const cancel = new bootstrap.Modal(cancelModal);
 
-const signupButtons = Array.from(document.querySelectorAll('.btn-signup'));
-const signInButtons = Array.from(document.querySelectorAll('.btn-signin'));
-const forgotButton = document.querySelector('.btn-forgot');
-const backToLogin = document.querySelector('.btn-back');
-const btnOTP = document.querySelector('.btn-otp');
+const btnCancel = document.querySelector('.btn-cancelappointment');
 
+btnCancel.addEventListener('click', showCancelModal);
 
-forgotButton.addEventListener('click', showForgotPassword);
-backToLogin.addEventListener('click', backToLog);
-btnOTP.addEventListener('click', showChangePassword);
-
-
-// SHOW SIGNUP
-function showForgotPassword() {
-    register.hide();
-    login.hide();
-    login.show();
-    forgot.show();
-}
-
-// BACK TO LOGIN FROM FORGOT PASSWORD
-function backToLog() {
-    forgot.hide();
-    register.hide();
-    change.hide();
-    login.show();
-}
-
-signupButtons.forEach(button => {
-    button.addEventListener('click', showSignup);
-});
-
-// SHOW SIGNUP
-function showSignup() {
-    forgot.hide();
-    change.hide();
-    login.hide();
-    register.show();
-}
-
-signInButtons.forEach(button => {
-    button.addEventListener('click', showSignin);
-});
-
-// SHOW SIGNUP
-function showSignin() {
-    forgot.hide();
-    change.hide();
-    register.hide();
-    login.show();
-}
-
-// SHOW SIGNUP
-function showChangePassword() {
-    forgot.hide();
-    change.hide();
-    register.hide();
-    login.hide();
-    change.show();
+// SHOW CANCEL-MANAGE
+function showCancelModal() {
+    manage.hide();
+    cancel.show();
 }
