b60c58ecd9a685cbcabb8ca47ecd9409e7b87326
it27427 <istiak.tushar.qtec@gmail.com>
2022-11-01 11:24:16 +0600

    added vanilla datepicker in myprofile

--- assets/js/datepicker/datepicker.js
+++ assets/js/datepicker/datepicker.js
@@ -45,10 +45,12 @@ class Calendar {
   createButtons() {
     const prev = document.createElement("button");
     prev.classList.add("button", "prev");
-    prev.innerHTML = "<i class='fas fa-chevron-left'></i>";
+    prev.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#121212" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>';
     const next = document.createElement("button");
     next.classList.add("button", "next");
-    next.innerHTML = "<i class='fas fa-chevron-right'></i>";
+    next.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>';
 
     prev.addEventListener("click", (e) => {
       e.preventDefault();
@@ -171,4 +173,4 @@ class Calendar {
   }
 }
 
-new Calendar(".date-input");
+new Calendar("#dob");
